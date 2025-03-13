936703f7a18b60b75ea98ae79ffc2c09b9d7cd27
chaudhari sunny <chaudharisunny6@gmail.com>
2025-03-13 12:29:36 +0530

    vercel file added

--- models/db.js
+++ models/db.js
@@ -1,8 +1,14 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb://localhost/inventory_management_system').then(()=>{
-    console.log('database connect');
-}).catch((error)=>{
-    console.log(error);
+
+const connectDB=async()=>{
     
-})
+    mongoose.connect('mongodb://localhost/inventory_management_system').then(()=>{
+        console.log('database connect');
+    }).catch((error)=>{
+        console.log(error);
+        
+    })
+}
+
+module.exports=connectDB
--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const app = express()
 const port = 3000
 const indexRoutes=require('./routes/index')
 const cookieParser=require('cookie-parser')
-const db=require('./models/db')
+const connectDB=require('./models/db')
 
+//connect database
+connectDB()
+
+//middleware
 app.use(express.json())
 app.use(cookieParser())
 app.get('/', (req, res) => res.send('inventory'))
+
+//main routes
 app.use('/api/v1/',indexRoutes)
 
+//server connect
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
